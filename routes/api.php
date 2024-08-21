15eadadbcd86fcfc71edb145f3253d4cbcd6a18b
Irfan858 <irfankurniawan858@gmail.com>
2024-08-21 16:36:26 +0700

    feat: Finishing API Controller At Post, Event, And Slider

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Posts
+Route::get('/post', [App\Http\Controllers\Api\PostController::class, "index"]);
+Route::get('/post/{id?}', [App\Http\Controllers\Api\PostController::class, "show"]);
+Route::get('/homepage/post', [App\Http\Controllers\Api\PostController::class, "PostHomePage"]);
+
+// Events
+Route::get('/event', [App\Http\Controllers\Api\EventController::class, "index"]);
+Route::get('/event/{slug?}', [App\Http\Controllers\Api\EventController::class, "show"]);
+Route::get('/homepage/event', [App\Http\Controllers\Api\EventController::class, "EventHomePage"]);
+
+// Slider
+Route::get('/slider', [App\Http\Controllers\Api\SliderController::class, "index"]);
